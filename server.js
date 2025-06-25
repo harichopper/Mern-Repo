c1f25968de4bc24e1df35c9e735b57c90e616cbb
HARIHARAN R <144225049+harichopper@users.noreply.github.com>
2025-06-25 13:27:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 const Contact = require("./models/Contact");
-const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
-// MongoDB connection
-mongoose.connect("mongodb+srv://haricdonh:hari5678@haricluster.0tsnw.mongodb.net/", {
+// Serve HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB connected"))
-.catch((err) => console.error(err));
+.catch((err) => console.error("MongoDB error:", err));
 
-// Route to handle contact form
+// POST form
 app.post("/contact", async (req, res) => {
   const { Name, email, message } = req.body;
-
   try {
-    const contactEntry = new Contact({ name: Name, email, message });
-    await contactEntry.save();
+    await Contact.create({ name: Name, email, message });
     res.send("<h2>Thanks for contacting us!</h2><a href='/'>Back</a>");
   } catch (err) {
-    res.status(500).send("Something went wrong!");
+    res.status(500).send("Error saving contact.");
   }
 });
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
